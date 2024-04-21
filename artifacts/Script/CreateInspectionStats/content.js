cc0873f7188ff7bcc9ac14f034fc56ad9b8c24f1
Svein Gunnar Larsen <svein.gunnar.larsen@neptune-software.com>
2024-04-21 20:19:28 +0200

    Init

---
+++ artifacts/Script/CreateInspectionStats/content.js
@@ -0,0 +1,37 @@
+var all = await entities.inspections.find();
+//console.log("ALL:: ", all); //Interesting the this does not console.log all the data, only first 2
+//console.log("ALL:: ", all.length); 
+
+var inspection_ct = {
+    complete : 0,
+    submitted : 0,
+    assigned : 0,
+    rejected : 0
+}
+
+all.forEach(inspection => {
+    if(inspection.status === "Complete"){
+        inspection_ct.complete ++;
+    }
+    else if(inspection.status === "Assigned"){
+        inspection_ct.assigned ++;
+    }
+    else if(inspection.status === "Submitted"){
+        inspection_ct.submitted ++;
+    }
+    else{
+        inspection_ct.rejected ++;
+    }
+})
+
+result = inspection_ct;
+
+console.log("Completion Dict:: ", inspection_ct);
+
+
+
+// Pieces of equipment
+
+//Could get them all individually like this, but thats no bueno. Muchas llamadas --JB
+// var completed_ct = await entities.inspections.count({status: "Complete"});
+// console.log("Complete Count:: ", completed_ct);
